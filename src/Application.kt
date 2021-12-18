8926f9ae144d11bee138fb23f2c2970950bfa1da
gamalragab <jrjb079@gmail.com>
2021-12-18 21:37:29 +0200

    Domne

--- src/Application.kt
+++ src/Application.kt
@@ -88,7 +88,7 @@ fun Application.module(testing: Boolean = false) {
         userRoute(userRepository, tokenManager)
         restaurantRoute(restaurantRepository, tokenManager)
         categoryAndProductRoute(categoryAndProductRepository)
-        get("/gi") {
+        get("/") {
             call.respondText("HELLO WORLD! this shop api create by Gamal ragab ", contentType = ContentType.Text.Plain)
         }
 
