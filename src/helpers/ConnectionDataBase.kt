8673ff40ce496243d3474a027b60d4876dc46471
gamalragab <jrjb079@gmail.com>
2021-12-18 21:15:08 +0200

    Done

--- src/helpers/ConnectionDataBase.kt
+++ src/helpers/ConnectionDataBase.kt
@@ -5,10 +5,17 @@ import org.ktorm.database.Database
 object ConnectionDataBase {
 
     val database = Database.connect(
-        url = "jdbc:mysql://127.0.0.1:3306/foodDoor",
+        url = "jdbc:mysql://MYSQL5037.site4now.net/db_a7ce11_noteapp", //server/database
         driver = "com.mysql.cj.jdbc.Driver",
-        user = "root",
-        password = "Gamal@@2172001"
+        user = "a7ce11_noteapp", // uid
+        password = "Gamal2172001"
+
+
+//        url = "jdbc:mysql://127.0.0.1:3306/foodDoor",
+//        driver = "com.mysql.cj.jdbc.Driver",
+//        user = "root",
+//        password = "Gamal@@2172001"
+//    )
     )
 
 }
